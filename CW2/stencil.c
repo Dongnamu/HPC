7ea4a6a86f9277b7192d4f6fc52ca6b01c25d427
David Shin <danwoo0911@gmail.com>
2018-12-10 18:30:09 +0000

    H

--- CW2/stencil.c
+++ CW2/stencil.c
@@ -100,7 +100,7 @@ int main(int argc, char *argv[]) {
   printf("rank: %d\n\tnorth=%d\n\tsouth=%d\n\teast=%d\n\twest=%d\n", rank,north,south,east,west);
 
   if (rank == (size - 1)) {
-    printf("I am here\n");
+    printf("I am rank %d\n", rank);
   }
 
 
