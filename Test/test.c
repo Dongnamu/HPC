9e4a2d391c67d3d3643144dabf17eadbca7c7de7
David Shin <danwoo0911@gmail.com>
2018-12-10 12:43:16 +0000

    H

--- Test/test.c
+++ Test/test.c
@@ -4,21 +4,21 @@
 #include "mpi.h"
 
 int main(int argc, char *argv[]) {
-  int ii,jj;             /* row and column indices for the grid */
-  int kk;                /* index for looping over ranks */
+  // int ii,jj;             /* row and column indices for the grid */
+  // int kk;                /* index for looping over ranks */
   int rank;              /* the rank of this process */
-  int left;              /* the rank of the process to the left */
-  int right;             /* the rank of the process to the right */
+  // int left;              /* the rank of the process to the left */
+  // int right;             /* the rank of the process to the right */
   int size;              /* number of processes in the communicator */
-  int tag = 0;           /* scope for adding extra information to a message */
-  MPI_Status status;     /* struct used by MPI_Recv */
-  int local_nrows;       /* number of rows apportioned to this rank */
-  int local_ncols;       /* number of columns apportioned to this rank */
-  int remote_ncols;      /* number of columns apportioned to a remote rank */
-  double *w;             /* local temperature grid at time t     */
-  double *sendbuf;       /* buffer to hold values to send */
-  double *recvbuf;       /* buffer to hold received values */
-  double *printbuf;      /* buffer to hold values for printing */
+  // int tag = 0;           /* scope for adding extra information to a message */
+  // MPI_Status status;     /* struct used by MPI_Recv */
+  // int local_nrows;       /* number of rows apportioned to this rank */
+  // int local_ncols;       /* number of columns apportioned to this rank */
+  // int remote_ncols;      /* number of columns apportioned to a remote rank */
+  // double *w;             /* local temperature grid at time t     */
+  // double *sendbuf;       /* buffer to hold values to send */
+  // double *recvbuf;       /* buffer to hold received values */
+  // double *printbuf;      /* buffer to hold values for printing */
   char hostname[MPI_MAX_PROCESSOR_NAME];  /* character array to hold hostname running process */
   int strlen;             /* length of a character array */
 
