5b10e0b709ae8026980f3594cf636e19bea8d98f
David Shin <danwoo0911@gmail.com>
2018-12-10 12:57:23 +0000

    H

--- Test/test.c
+++ Test/test.c
@@ -39,8 +39,8 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank );
 
-  printf("Hello, world; from host %s: process %d of %d\n", hostname, rank, size);
-
+  // printf("Hello, world; from host %s: process %d of %d\n", hostname, rank, size);
+  printf("Printing\n");
   MPI_Finalize();
 
   return EXIT_SUCCESS;
