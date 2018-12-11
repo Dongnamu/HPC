87cfaad61f34afb6c3e3e628294f9ec8cfad8c87
David Shin <danwoo0911@gmail.com>
2018-12-11 19:57:14 +0000

    H

--- CW2/stencil.c
+++ CW2/stencil.c
@@ -303,8 +303,8 @@ int main(int argc, char *argv[]) {
   printf("------------------------------------\n");
 
 
-  if (rank == 2){
-    output_image("RANK2.pgm", local_nrows, local_ncols, image_pad);
+  if (rank == 1){
+    output_image("RANK1.pgm", local_nrows, local_ncols, image_pad);
   }
 
   free(image);
