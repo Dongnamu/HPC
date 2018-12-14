5280043c1d5c0cbabfc02bb373f972b895ea5d68
Jong Shin <js16397@newblue1.cm.cluster>
2018-12-14 05:38:44 +0000

    script file

---
+++ CW2/env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+module load languages/intel-compiler-16-u2 openmpi/intel/64/1.6.5 tools/intel_builds/tau-2.23.1-openmpi intel-cluster-studio/compiler/64/13.1/117 intel-cluster-studio/mkl/64/13.1/117 languages/python-2.7.6
